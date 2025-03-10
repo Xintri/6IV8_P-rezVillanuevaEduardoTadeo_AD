c828f57d78c2eb5bbfc7691908f8665744619b01
Xintri_ <mimosa.kuko@gmail.com>
2025-03-10 16:23:36 -0600

    04Pandas Ejemplo

---
+++ ElementosBasicosEstaduscutca/04Pandas.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv('./ElementosBasicosEstaduscutca/housing.csv')
+
+##Mostrar las primeras 5 filas
+
+print(df.head())
+
+##Mostrar las ultimas 5 filas
+
+print(df.tail())
+
+##Fila en especifico
+
+print(df.iloc[7])
+
+##mostrar columna ocean_proximity
+print(df["ocean_proximity"])
+
+##Mostrar la media de la columna total_rooms
+media_cuarto = df["total_rooms"].mean()
+print("Media de cuartos: " + str(media_cuarto))
+
+##Mediana
+mediana_cuarto = df["median_house_value"].median()
+print("La mediana de cuartos: " + str(mediana_cuarto))
+
+##Suma de popular
+salarioTotal = df["population"].sum()
+print("El salario total es de: " + str(salarioTotal))
+
+##Filtrar
+vamoshacerfiltro = df[df["ocean_proximity"] == "ISLAND"]
+print(vamoshacerfiltro)
