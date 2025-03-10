72c6ed1fc8d18f3a5a1059928ee385ad170d2528
Xintri_ <mimosa.kuko@gmail.com>
2025-03-10 15:38:12 -0600

    02Diccionario ejemplo

---
+++ ElementosBasicosEstaduscutca/Diccionario.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+##Escribir una funcion que reciva un diccionario con las notas de los estudiantes del curso y devuelve una serie con minimo, maximo, media, desviacion critica
+
+def estadistica_notas(notas):
+    notas = pd.Series(notas)
+    estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index = ['Minimo', 'Maximo', 'Media', 'Desviacion estandar'])
+
+    return estadisticas
+
+
+notas = {"Jaun": 0, "Juana": 10, "Pepe":9 , "Roman":1, "Maximiliano": 8, "Envi":7, "Icarus":6}
+
+print(estadistica_notas(notas))
